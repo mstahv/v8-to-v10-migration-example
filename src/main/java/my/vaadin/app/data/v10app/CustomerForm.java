00c15128e719ed434111f7c6f21a131f455b3d39
Matti Tahvonen <matti@vaadin.com>
2018-08-22 11:29:56 +0300

    copy paste form, switch to v10 components, test using @Route

---
+++ src/main/java/my/vaadin/app/data/v10app/CustomerForm.java
@@ -0,0 +1,81 @@
+package my.vaadin.app.data.v10app;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.router.Route;
+import my.vaadin.app.v8app.*;
+import my.vaadin.app.data.CustomerStatus;
+import my.vaadin.app.data.CustomerService;
+import my.vaadin.app.data.Customer;
+
+@Route
+public class CustomerForm extends FormLayout {
+
+    private TextField firstName = new TextField("First name");
+    private TextField lastName = new TextField("Last name");
+    private TextField email = new TextField("Email");
+    private ComboBox<CustomerStatus> status = new ComboBox<>("Status");
+    private DatePicker birthdate = new DatePicker("Birthday");
+    private Button save = new Button("Save");
+    private Button delete = new Button("Delete");
+
+    private CustomerService service = CustomerService.getInstance();
+    private Customer customer;
+    private MainUI mainUI;
+    private Binder<Customer> binder = new Binder<>(Customer.class);
+    
+    
+    public CustomerForm() {
+        this(null);
+    }
+
+    public CustomerForm(MainUI myUI) {
+        this.mainUI = myUI;
+
+        setSizeUndefined();
+        HorizontalLayout buttons = new HorizontalLayout(save, delete);
+        add(firstName, lastName, email, status, birthdate, buttons);
+
+        status.setItems(CustomerStatus.values());
+//        save.setStyleName(ValoTheme.BUTTON_PRIMARY);
+//        save.setClickShortcut(KeyCode.ENTER);
+
+        binder.bindInstanceFields(this);
+
+        save.addClickListener(e -> this.save());
+        delete.addClickListener(e -> this.delete());
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+        binder.setBean(customer);
+
+        // Show delete button for only customers already in the database
+        delete.setVisible(customer.isPersisted());
+        setVisible(true);
+        firstName.focus();
+    }
+
+    private void delete() {
+        service.delete(customer);
+        setVisible(false);
+        notifyMainView();
+    }
+
+    private void save() {
+        service.save(customer);
+        notifyMainView();
+    }
+    
+    private void notifyMainView() {
+        if(mainUI != null) {
+            mainUI.updateList();
+        }
+    }
+
+}
